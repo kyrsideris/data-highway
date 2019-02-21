5389570d01074139a8b430dfcef5ff098a036629
Kyriakos Sideris <k.sideris@gmail.com>
2019-02-21 12:40:57 +0000

    Added variations for onMessage in NORMAL road

--- endpoint/onramp/api/src/test/java/com/hotels/road/onramp/api/OnrampImplTest.java
+++ endpoint/onramp/api/src/test/java/com/hotels/road/onramp/api/OnrampImplTest.java
@@ -20,6 +20,7 @@ import static java.util.Collections.singletonMap;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.mock;
@@ -40,13 +41,14 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.primitives.Ints;
 
 import com.hotels.road.model.core.InnerMessage;
 import com.hotels.road.model.core.Road;
 import com.hotels.road.model.core.SchemaVersion;
+import com.hotels.road.rest.model.RoadType;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OnrampImplTest {
@@ -55,55 +57,92 @@ public class OnrampImplTest {
   private Schema schema = SchemaBuilder.record("test_message").fields().requiredInt("id").endRecord();
   private SchemaVersion schemaVersion = new SchemaVersion(schema, 1, false);
 
-  private @Mock Road road;
+  private @Mock Road roadNormal;
+  private @Mock Road roadCompact;
   private @Mock OnrampSender sender;
   private @Mock Random random;
 
-  private OnMessage onMessage;
+  private OnMessage msgNullKey;
+  private OnMessage msgNullMessage;
+  private OnMessage msgAllNull;
+  private OnMessage msgAllNotNull;
   private OnrampImpl underTest;
 
   @Before
   public void setUp() {
-    when(road.getSchemas()).thenReturn(singletonMap(schemaVersion.getVersion(), schemaVersion));
-    when(road.getPartitionPath()).thenReturn("$.udt.user.guid");
-    when(sender.getPartitionCount(eq(road))).thenReturn(1);
+    when(roadNormal.getType()).thenReturn(RoadType.NORMAL);
+    when(roadNormal.getSchemas()).thenReturn(singletonMap(schemaVersion.getVersion(), schemaVersion));
+    when(roadNormal.getPartitionPath()).thenReturn("$.udt.user.guid");
+    when(sender.getPartitionCount(eq(roadNormal))).thenReturn(1);
     underTest = mock(
         OnrampImpl.class,
-        withSettings().useConstructor(road, sender, random).defaultAnswer(CALLS_REAL_METHODS));
-    onMessage = new OnMessage(null, mapper.createObjectNode().put("id", 123));
+        withSettings().useConstructor(roadNormal, sender, random).defaultAnswer(CALLS_REAL_METHODS));
+
+    ObjectNode value = mapper.createObjectNode().put("id", 123);
+    msgNullKey     = new OnMessage(null, value);
+    msgNullMessage = new OnMessage("my key", null);
+    msgAllNull     = new OnMessage(null, null);
+    msgAllNotNull  = new OnMessage("my key", value);
   }
 
   @Test
-  public void sendEvent_calls_sender_correctly() throws Exception {
+  public void sendOnMessage_calls_sender_correctly_onNormalRoad() throws Exception {
     ArgumentCaptor<InnerMessage> innerMsgCaptor = ArgumentCaptor.forClass(InnerMessage.class);
     Future<Boolean> future = CompletableFuture.completedFuture(true);
-    when(sender.sendInnerMessage(eq(road), innerMsgCaptor.capture())).thenReturn(future);
+    when(sender.sendInnerMessage(eq(roadNormal), innerMsgCaptor.capture())).thenReturn(future);
 
-    Future<Boolean> result = underTest.sendOnMessage(onMessage, Instant.now());
+    Instant time = Instant.now();
+    Future<Boolean> resultNullKey = underTest.sendOnMessage(msgNullKey, time);
 
-    assertThat(result, is(future));
+    assertThat(resultNullKey, is(future));
 
     InnerMessage innerMessage = innerMsgCaptor.getValue();
     assertThat(innerMessage.getPartition(), is(0));
+    assertThat(innerMessage.getTimestampMs(), is(time.toEpochMilli()));
     assertNull(innerMessage.getKey());
     byte[] message = innerMessage.getMessage();
     assertThat(message[0], is((byte) 0x00));
     assertThat(Ints.fromBytes(message[1], message[2], message[3], message[4]), is(1));
+
+    Future<Boolean> resultNullMessage = underTest.sendOnMessage(msgNullMessage, time);
+    try {
+      resultNullMessage.get();
+      fail("Exception was not raised for null message on OnMessage!");
+    } catch (Exception e) {
+      assertThat(
+          e.getMessage(),
+          is("com.hotels.road.exception.InvalidEventException: "
+              + "The event failed validation. Normal road messages must contain a message"));
+    }
+
+    Future<Boolean> resultAllNull = underTest.sendOnMessage(msgAllNull, time);
+    try {
+      resultAllNull.get();
+      fail("Exception was not raised for null key and message on OnMessage!");
+    } catch (Exception e) {
+      assertThat(
+          e.getMessage(),
+          is("com.hotels.road.exception.InvalidEventException: "
+              + "The event failed validation. Normal road messages must contain a message"));
+    }
+
+    Future<Boolean> resultAllNotNull = underTest.sendOnMessage(msgAllNotNull, time);
+    assertThat(resultAllNotNull, is(future));
   }
 
   @Test
   public void roadIsAvailable() {
-    when(road.isEnabled()).thenReturn(true);
+    when(roadNormal.isEnabled()).thenReturn(true);
 
     assertThat(underTest.isAvailable(), is(true));
-    assertThat(underTest.getRoad(), is(road));
+    assertThat(underTest.getRoad(), is(roadNormal));
   }
 
   @Test
   public void roadIsNotAvailable() {
-    when(road.isEnabled()).thenReturn(false);
+    when(roadNormal.isEnabled()).thenReturn(false);
 
     assertThat(underTest.isAvailable(), is(false));
-    assertThat(underTest.getRoad(), is(road));
+    assertThat(underTest.getRoad(), is(roadNormal));
   }
 }
